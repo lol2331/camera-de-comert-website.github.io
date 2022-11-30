94e7b277a477607e17371d94bdd4414a12de5cec
lol2331 <114163487+lol2331@users.noreply.github.com>
2022-11-30 22:17:25 +0200

    Add files via upload

---
+++ Lerning.js
@@ -0,0 +1,20 @@
+var a;
+var b = 5;
+var str = "Ana are mere"
+var array = [["Dick", 69] + ["Pussy", 0]]
+
+function say_bullshit(bullshit){
+    console.log(bullshit)
+}
+
+say_bullshit("vrum vrum")
+
+var str_length = str.length
+var frst_letter = str[0]
+var last_letter = str[str_length-1]
+
+a = 9;
+
+b=a;
+
+console.log(b)
--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 let table = document.getElementById("table")
+let table2 = document.getElementById("table2")
 onload = table.style.display = "none"
+onload  = table.style.display = "none"
 
 function searchBlog(){
     
     let filter = document.getElementById("filter").value.toUpperCase();
     let table = document.getElementById("table")
     let tr = table.getElementsByTagName("tr")
+    
 
     
 
     for(var i = 0; i < tr.length; i++){
         let td = tr[i].getElementsByTagName("td")[0];
+        
 
 
         if(td){
             let text_value = td.textContent || td.innerHTML;
+            
 
             if(text_value.toUpperCase().indexOf(filter) > -1){
                 table.style.display = "";
@@ -27,7 +32,12 @@ function searchBlog(){
             
             
         }
+            
+            
+        
     }
+ 
+    
 }
 
 
