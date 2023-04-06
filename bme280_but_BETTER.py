c50970ba140e7025d8692034aadc85581b49b91b
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-04-06 13:45:42 +0300

    eveniment ceva ceva

---
+++ bme280_but_BETTER.py
@@ -0,0 +1,19 @@
+import smbus2
+import bme280
+
+# Define I2C bus and BME280 address
+bus = smbus2.SMBus(1)
+address = 0x76
+
+# Initialize the BME280 sensor
+calibration_params = bme280.load_calibration_params(bus, address)
+
+# Read temperature, humidity, and pressure data
+data = bme280.sample(bus, address, calibration_params)
+temp = data.temperature
+hum = data.humidity
+pres = data.pressure
+
+print("Temperature: {:.2f} C".format(temp))
+print("Humidity: {:.2f} %".format(hum))
+print("Pressure: {:.2f} hPa".format(pres))
