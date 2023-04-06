a0425f081ac4377c435088c9887edfc2a5fe9c42
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-04-06 13:53:01 +0300

    ma omor

--- bme280_but_BETTER.py
+++ bme280_but_BETTER.py
@@ -5,14 +5,16 @@ import bme280
 bus = smbus2.SMBus(1)
 address = 0x76
 
-# Initialize the BME280 sensor
-bme280.setup(bus, address)
+# Load calibration parameters from the BME280 sensor
+calibration_params = bme280.load_calibration_params(bus, address)
+
+# Initialize the BME280 sensor with the loaded calibration parameters
+bme280_data = bme280.sample(bus, address, calibration_params)
 
 # Read temperature, humidity, and pressure data
-data = bme280.readData()
-temp = data.temperature
-hum = data.humidity
-pres = data.pressure
+temp = bme280_data.temperature
+hum = bme280_data.humidity
+pres = bme280_data.pressure
 
 print("Temperature: {:.2f} C".format(temp))
 print("Humidity: {:.2f} %".format(hum))
