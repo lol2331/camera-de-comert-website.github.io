43474d9e8d1ea80c6ca09e2591b39cc0fe49b30e
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-04-06 14:21:36 +0300

    Update and rename bme280_but_BETTER.py to imu.py

---
+++ imu.py
@@ -0,0 +1,35 @@
+import smbus
+import time
+
+# MPU-6050 register addresses
+PWR_MGMT_1 = 0x6B
+ACCEL_XOUT_H = 0x3B
+GYRO_XOUT_H = 0x43
+
+# Initialize I2C bus and MPU-6050
+bus = smbus.SMBus(1)
+bus.write_byte_data(0x68, PWR_MGMT_1, 0)
+
+# Read and print accelerometer and gyroscope data
+while True:
+    accel_x = read_word_2c(ACCEL_XOUT_H)
+    accel_y = read_word_2c(ACCEL_YOUT_H)
+    accel_z = read_word_2c(ACCEL_ZOUT_H)
+    gyro_x = read_word_2c(GYRO_XOUT_H)
+    gyro_y = read_word_2c(GYRO_YOUT_H)
+    gyro_z = read_word_2c(GYRO_ZOUT_H)
+
+    print("accelerometer (x,y,z): ({},{},{})".format(accel_x, accel_y, accel_z))
+    print("gyroscope (x,y,z): ({},{},{})".format(gyro_x, gyro_y, gyro_z))
+
+    time.sleep(0.1)
+
+def read_word_2c(addr):
+    high = bus.read_byte_data(0x68, addr)
+    low = bus.read_byte_data(0x68, addr + 1)
+    val = (high << 8) + low
+
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
